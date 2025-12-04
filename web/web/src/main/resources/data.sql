8e49b9b747aaf87b0b2c9c0a5dc5aa09fb9dd5b7
laucss <laucubsan@alum.us.es>
2025-12-04 14:06:09 +0100

    product corregido

---
+++ web/web/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO productos(id,nombre,descripcion,precio,fecha_caducidad, categoria) VALUES
+     (1,'Manzana','manzana pink lady', 0.5,'2025-12-26', 'FRESCOS'),
+     (2,'Croquetas de Jamón','croquetas de jamón marca DIA', 4.75,'2026-02-26', 'CONGELADOS'),
+     (3,'Arroz bomba','arroz bomba marca La Fallera', 5.10,'2026-12-26', 'DESPENSA'),
+     (4,'Zumo de piña','zumo de piña recién exprimido', 3.80,'2025-12-15', 'BEBIDAS'),
+     (5,'Lejía','lejía blanca de baños', 3.10,null, 'HOGAR'),
+     (6,'Jabón de manos','jabón de manos olor lavanda', 2.20,null, 'CUIDADO_PERSONAL');
