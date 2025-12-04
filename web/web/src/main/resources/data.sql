3d411e15f1088f958eed44fb59ba526669bc8a39
laucss <laucubsan@alum.us.es>
2025-12-04 22:01:05 +0100

    dos usuarios registrados

--- web/web/src/main/resources/data.sql
+++ web/web/src/main/resources/data.sql
@@ -6,3 +6,18 @@ INSERT INTO productos(id,nombre,descripcion,precio,fecha_caducidad, categoria) V
      (4,'Zumo de piña','zumo de piña recién exprimido', 3.80,'2025-12-15', 'BEBIDAS'),
      (5,'Lejía','lejía blanca de baños', 3.10,null, 'HOGAR'),
      (6,'Jabón de manos','jabón de manos olor lavanda', 2.20,null, 'CUIDADO_PERSONAL');
+
+
+
+INSERT INTO users(id,username,password,role) VALUES
+    (1,'jefe','jefe','JEFE'),
+    (2,'empleado1','empleado1','EMPLEADO'),
+    (3,'empleado2','empleado2','EMPLEADO');
+
+
+/*
+INSERT INTO users(id,username,password,role) VALUES
+    (1,'jefe','$2a$10$7QJf1o8e0G6H9c4cX9bZ1u5F8Kqz1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z','JEFE'),
+    (2,'empleado1','$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36c6B8m7r9g6r9g6r9g6r9g6','EMPLEADO'),
+    (3,'empleado2','$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36c6B8m7r9g6r9g6r9g6r9g6','EMPLEADO');
+*/
